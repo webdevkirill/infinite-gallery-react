4345a7325aba1e6087730e701655815ea1e40f04
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-22 17:49:04 +0300

    Удаляет ненужные файлы и добавляет статичную верстку

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	return (
+		<div className='app'>
+			<h1>Бесконечная галерея Unsplash</h1>
 
-export default App;
+			<form>
+				<input type='text' placeholder='Поиск в Unsplash...' />
+				<button>Найти</button>
+			</form>
+
+			<div className='image-grid'>
+				{[...Array(100)].map((_, index) => (
+					<div className='image' key={index}>
+						<img
+							src='https://placekitten.com/g/1920/1080'
+							alt='Sample'
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
