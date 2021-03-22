d607ef432bc36479d6a0578c52afdd897a005b8f
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-22 18:11:41 +0300

    Добавляет загрузку картинок с unsplah и ошибку, если не добавлен API клч

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 export default function App() {
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		fetch(`https://api.unsplash.com/photos?client_id=${accessKey}`)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setImages(data);
+			});
+	}, []);
+
+	if (!accessKey) {
+		return (
+			<a href='https://api.unsplash.com/developers' className='error'>
+				Получите API ключ и добавьте его в .env файл
+			</a>
+		);
+	}
+
 	return (
 		<div className='app'>
 			<h1>Бесконечная галерея Unsplash</h1>
