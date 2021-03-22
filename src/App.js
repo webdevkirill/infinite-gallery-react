c7655b450a06e070bca386666c50ebeda84653d9
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-22 18:32:35 +0300

    Добавляет бесконечную загрузку при скролле

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
@@ -7,13 +8,16 @@ export default function App() {
 	const [images, setImages] = useState([]);
 
 	useEffect(() => {
+		getPhotos();
+	}, []);
+
+	const getPhotos = () => {
 		fetch(`https://api.unsplash.com/photos?client_id=${accessKey}`)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
-				setImages(data);
+				setImages((images) => [...images, ...data]);
 			});
-	}, []);
+	};
 
 	if (!accessKey) {
 		return (
@@ -32,16 +36,29 @@ export default function App() {
 				<button>Найти</button>
 			</form>
 
-			<div className='image-grid'>
-				{images.map((image) => (
-					<div className='image' key={image.id}>
-						<img
-							src={image.urls.regular}
-							alt={image.alt_description}
-						/>
-					</div>
-				))}
-			</div>
+			<InfiniteScroll
+				dataLength={images.length} //This is important field to render the next data
+				next={getPhotos}
+				hasMore={true}
+				loader={<h4>Loading...</h4>}
+			>
+				<div className='image-grid'>
+					{images.map((image) => (
+						<a
+							href={image.links.html}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='image'
+							key={image.id}
+						>
+							<img
+								src={image.urls.regular}
+								alt={image.alt_description}
+							/>
+						</a>
+					))}
+				</div>
+			</InfiniteScroll>
 		</div>
 	);
 }
